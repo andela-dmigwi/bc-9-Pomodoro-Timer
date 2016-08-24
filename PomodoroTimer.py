4796984fb62a9770ecab24a8c4b75585c9a2f17a
Migwi-Ndungu <migwindungu0@gmail.com>
2016-08-24 14:35:40 +0300

    (Feature) Implentation of both Pomodoro_command module and PomodoroTimer module as both hold the cliff implementation frame work
    Done #12 Done #12

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -1,5 +1,45 @@
-'''
-This module will hold main file that will be run.
-This is where the cliff implementation begins from
+import sys
+import logging
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+from pomodoro_command import MyCommand
 
-''' 
+class PomodoroTimer(App):
+    '''
+	This module will hold main file that will be run.
+	This is where the cliff implementation begins from
+    '''
+
+    log = logging.getLogger(__name__)
+    def __init__(self):
+        command = CommandManager('PomodoroTimer.app')
+        super(PomodoroTimer, self).__init__(
+            description = 'sample app',
+            version = '0.1',
+            command_manager = command,
+        )
+        commands = {'pomodoro': MyCommand}
+
+        for k, v in commands.iteritems():
+            command.add_command(k, v)
+
+
+    def initialize_app(self, argv):
+        self.log.debug('initialize_app')
+
+    def prepare_to_run_command(self, cmd):
+        self.log.debug('prepare_to_run_command %s', cmd.__class__.__name__)
+
+    def clean_up(self, cmd, result, err):
+        self.log.debug('clean_up %s', cmd.__class__.__name__)
+        if err:
+            self.log.debug('got an error: %s', err)
+
+
+def main(argv=sys.argv[1:]):
+    app = PomodoroTimer()
+    return app.run(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
--- pomodoro_command.py
+++ pomodoro_command.py
@@ -1,7 +1,25 @@
-'''
-This Module will hold the cliff implementation that retrieves the
-various command input by the user
-This module also will hold cliff framework implementation although 
-with some modification
+import os
+import sys
+import logging
+from cliff.command import Command
 
-'''
+class MyCommand(Command):
+    '''
+    This Module will hold the cliff implementation that retrieves the
+    various command input by the user
+    This module also will hold cliff framework implementation although 
+    with some modification
+    '''
+
+    log = logging.getLogger(__name__)
+     
+    #parses the args passed and returns them
+    def get_parser(self, prog_name):
+        parser = super(MyCommand, self).get_parser(prog_name)
+        parser.add_argument('arg', nargs='?', default=None)
+
+        return parser
+
+    #picks the parsed arguments and uses them
+    def take_action(self, parsed_args):
+        self.app.stdout.write(str(parsed_args.arg) + "\n")
